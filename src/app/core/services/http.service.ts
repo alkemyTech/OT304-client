912bb1d9b19274eb6003306d277760819d7bac8a
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2022-09-15 18:35:34 -0500

    Nuevo servicio para peticiones http componentes de contacto creado exitosamente

--- src/app/core/services/http.service.ts
+++ src/app/core/services/http.service.ts
@@ -16,5 +16,13 @@ export class HttpService {
   public get<T>(url: string, activateHeader:boolean = false ):Observable<T> {
     return this.http.get<T>(url, activateHeader ? { headers: this._headers }: {});
   }
+
+  public post<T>(url: string, activateHeader:boolean = false, body:any ):Observable<T> {
+    return this.http.post<T>(url, body ,activateHeader ? { headers: this._headers }: {});
+  }
+
+  public put<T>(url: string, activateHeader:boolean = false,body:any ):Observable<T> {
+    return this.http.put<T>(url, body, activateHeader ? { headers: this._headers }: {});
+  }
 }
 
