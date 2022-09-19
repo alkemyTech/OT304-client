9c755ba33449f56f52806e22aaff46daf83784f6
yisus1002 <yisusgarcia1002@gmail.com>
2022-09-18 21:22:58 -0500

    Se creo la ruta para editar datos de la organización

--- src/app/features/app-routing.module.ts
+++ src/app/features/app-routing.module.ts
@@ -2,11 +2,15 @@ import { ActivityFormComponent } from "./pages/activities/activity-form/activity
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
+import { EditComponent } from "./pages/backoffice/organization/edit/edit.component";
 
 const routes: Routes = [
   { 
     path: "actividades", 
     component: ActivityFormComponent },
+  { 
+    path: "backoffice/organization/edit", 
+    component: EditComponent},
   {
     path: "",
     redirectTo: "actividades",
