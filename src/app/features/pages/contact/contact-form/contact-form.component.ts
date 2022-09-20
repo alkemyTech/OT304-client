c379e4dc550444c3616ce8111b25494ecdba7181
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2022-09-19 20:26:18 -0500

    Añadiendo la libreria de iiconos de font awesome y añadiendo la validacion para el campo de telefono

--- src/app/features/pages/contact/contact-form/contact-form.component.ts
+++ src/app/features/pages/contact/contact-form/contact-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,Validators,AbstractControl,ValidationErrors } from '@angular/forms';
+import { faInfoCircle,faCheck,faExclamation,IconDefinition } from "@fortawesome/free-solid-svg-icons"
+import { WrongDataException } from 'src/app/core/lib';
 
 @Component({
   selector: 'app-contact-form',
@@ -6,10 +9,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
+  //form
+  contactForm:FormGroup;
 
-  constructor() { }
+  //Font Awesome Icons
+  faInfoCircle:IconDefinition = faInfoCircle;
+  faCheck:IconDefinition = faCheck;
+  faExclamation:IconDefinition = faExclamation;
+
+  constructor(private _formBuilder_:FormBuilder) { 
+    //Form Building
+    this.contactForm = this._formBuilder_.group({
+      name:["",[Validators.required,]],
+      email:["",[Validators.required, Validators.email]],
+      phone:["",[Validators.compose([Validators.minLength(8)]),Validators.required,this.isNumber]],
+      message:["",Validators.required]
+    })
+  }
 
   ngOnInit(): void {
+    
   }
+  //Sent Data testing
+  sendData(values:FormData):void{
+    console.log(values)
+  }
+  
+  get phone(){
+    return this.contactForm.get("phone");
+  }
+  //Validates if input value is a number
+  isNumber(control:AbstractControl):ValidationErrors|null{
+    const phone:string = control.value;
+    var isNum:boolean=true;
+    for(var i = 0; i<phone.length; i++){
+      var num = parseInt(phone[i]);
+      if(isNaN(num)){
+        isNum = false;
+        break;
+      }else{
+        isNum = true;
+      }
+    }
+
+      if(isNum === true)return null;
+      console.log(isNum)
+      return new WrongDataException("This field is able to receive only numbers");
+    
+    
+  }
+
+ 
 
 }
