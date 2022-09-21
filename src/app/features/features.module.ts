8737802e62db54169f86e34f53dd8310842c6e56
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2022-09-21 15:06:41 -0500

    añadiendo modulo de Material

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,6 +2,14 @@ import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { MaterialModule } from "./material.module";
+
+
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HeaderInterceptor } from "../core/interceptors/header.interceptor";
+
 import { ActivityFormComponent } from "./pages/activities/activity-form/activity-form.component";
 import { LoginFormComponent } from "./pages/auth/login-form/login-form.component";
 import { RegisterFormComponent } from "./pages/auth/register-form/register-form.component";
@@ -11,9 +19,8 @@ import { SlidesFormComponent } from "./pages/slides/slides-form/slides-form.comp
 import { TestimonialFormComponent } from "./pages/testimonials/testimonial-form/testimonial-form.component";
 import { UserFormComponent } from "./pages/users/user-form/user-form.component";
 import { ContactFormComponent } from './pages/contact/contact-form/contact-form.component';
-import { HTTP_INTERCEPTORS } from "@angular/common/http";
-import { HeaderInterceptor } from "../core/interceptors/header.interceptor";
 import { MainContactComponent } from './pages/contact/main-contact/main-contact.component';
+
 
 @NgModule({
   declarations: [
@@ -39,7 +46,7 @@ import { MainContactComponent } from './pages/contact/main-contact/main-contact.
     UserFormComponent,
     RouterModule
   ],
-  imports: [CommonModule, AppRoutingModule, RouterModule],
+  imports: [CommonModule, AppRoutingModule, RouterModule,ReactiveFormsModule, FontAwesomeModule, MaterialModule],
   providers:[
     {
       provide:HTTP_INTERCEPTORS,
