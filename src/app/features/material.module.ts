f72aa19b11d8734373c2de9276abb9bf7ea403cb
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2022-09-21 15:16:51 -0500

    creando un modulo de material design aparte para importar todos los modulos necesarios

---
+++ src/app/features/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+
+import{
+
+    MatFormFieldModule
+    
+
+} from '@angular/material/form-field'
+
+@NgModule({
+    imports:[
+        MatFormFieldModule
+    ],
+    exports:[
+        MatFormFieldModule
+    ]
+})
+
+export class MaterialModule{}
