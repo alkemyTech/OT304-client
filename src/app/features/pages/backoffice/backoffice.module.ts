3566ca2e65dadaaa13688c71a10116563e6dfe0c
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2022-09-22 12:26:41 -0300

    se creo componente homeBackoffice

--- src/app/features/pages/backoffice/backoffice.module.ts
+++ src/app/features/pages/backoffice/backoffice.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { BackofficeRoutingModule } from './backoffice-routing.module';
 import { BackofficeComponent } from './backoffice.component';
+import { HomeComponent } from './home/home.component';
+import { HomeBackofficeComponent } from './home-backoffice/home-backoffice.component';
 
 
 @NgModule({
   declarations: [
-    BackofficeComponent
+    BackofficeComponent,
+    HomeComponent,
+    HomeBackofficeComponent
   ],
   imports: [
     CommonModule,
