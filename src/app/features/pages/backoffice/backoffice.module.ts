d56d542aeb492e66bd6d33c568e4204044d07b30
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2022-09-22 14:20:56 -0300

    creacion de formulario para edicion de inicio

--- src/app/features/pages/backoffice/backoffice.module.ts
+++ src/app/features/pages/backoffice/backoffice.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { BackofficeRoutingModule } from './backoffice-routing.module';
 import { BackofficeComponent } from './backoffice.component';
-import { HomeComponent } from './home/home.component';
 import { HomeBackofficeComponent } from './home-backoffice/home-backoffice.component';
+
 
 
 @NgModule({
   declarations: [
     BackofficeComponent,
-    HomeComponent,
     HomeBackofficeComponent
   ],
   imports: [
