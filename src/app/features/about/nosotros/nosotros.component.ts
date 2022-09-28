435fab20cf04f80d92e5c30b1c20272e470eab40
master360 <18-10641@usb.ve>
2022-09-28 17:36:39 -0400

    Integrar widgets de Twitter a la pagina nosotros

--- src/app/features/about/nosotros/nosotros.component.ts
+++ src/app/features/about/nosotros/nosotros.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../../../core/services/http.service'
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "../../../core/services/http.service";
 
 @Component({
-  selector: 'app-nosotros',
-  templateUrl: './nosotros.component.html',
-  styleUrls: ['./nosotros.component.scss']
+  selector: "app-nosotros",
+  templateUrl: "./nosotros.component.html",
+  styleUrls: ["./nosotros.component.scss"],
 })
 export class NosotrosComponent implements OnInit {
-  public data: any =  []
+  public data: any = [];
 
-  constructor(private http:HttpService) { }
+  constructor(private http: HttpService) {}
 
   async ngOnInit(): Promise<any> {
-  await this.http.get('https://ongapi.alkemy.org/api/members').subscribe((response:any)=> {
-    this.data = response.data
-    console.log(response.data)})  
+    (<any>window).twttr.widgets.load();
+
+    await this.http
+      .get("https://ongapi.alkemy.org/api/members")
+      .subscribe((response: any) => {
+        this.data = response.data;
+        console.log(response.data);
+      });
   }
-
-
 }
