0ba99ec0bb43ac5744e09d88efc72f6412f18913
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2022-09-30 14:11:58 -0300

    cambios utilizando variables de entorno

--- src/app/core/services/news-home.service.ts
+++ src/app/core/services/news-home.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Slides } from '../lib/interfaces/entity.interfaces';
 import { HttpService } from './http.service';
 
@@ -12,23 +13,23 @@ export class NewsHomeService extends HttpService{
   constructor(http:HttpClient) {
     super(http);
   }
-
+//API_URL:'https://ongapi.alkemy.org/api/'
   public getSlides():Observable<any>{
-    return super.get<any>('https://ongapi.alkemy.org/public/api/slides',false);
+    return super.get<any>(environment.API_URL+'slides',false);
   }
    
   public getSlidesById(id:number): Observable<any>{
-    return super.get<any>('https://ongapi.alkemy.org/public/api/slides/'+id,false);
+    return super.get<any>(environment.API_URL+'slides/'+id,false);
   }
 
   public createSlides(body:Slides):Observable<any>{
-    return super.post<any>('https://ongapi.alkemy.org/public/api/slides/',false,body);
+    return super.post<any>(environment.API_URL+'slides/',false,body);
   }
   
   public editSlides(id:number,body:Slides):Observable<any>{
-    return super.put<any>('https://ongapi.alkemy.org/public/api/slides/'+id,false,body);
+    return super.put<any>(environment.API_URL+'slides/'+id,false,body);
   }
   public deleteSlides(id:number):Observable<any>{
-    return super.delete<any>('https://ongapi.alkemy.org/public/api/slides/'+id, false)
+    return super.delete<any>(environment.API_URL+'slides/'+id, false)
   }
 }
