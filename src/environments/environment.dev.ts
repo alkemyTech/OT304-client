7fa929a2d8ed7528567929c7a3c5ba0d9d084152
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2022-09-29 20:50:43 -0500

    añadiendo nuevo entorno en el aplicativo

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+//Development mode
+
+export const environment = {
+    production: false,
+    baseApiUrl: 'https://ongapi.alkemy.org/public/api/',
+    title: 'I finally know how to use an environment'
+  };
