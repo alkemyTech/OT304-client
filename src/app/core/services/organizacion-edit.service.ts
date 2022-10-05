2d4206cf7c5abc0345e769039502b0e9d7543db4
yisus1002 <yisusgarcia1002@gmail.com>
2022-10-05 16:54:49 -0500

    Se agrego el metodo para enviar una imagen

--- src/app/core/services/organizacion-edit.service.ts
+++ src/app/core/services/organizacion-edit.service.ts
@@ -7,6 +7,8 @@ import { Observable } from 'rxjs';
 })
 export class OrganizacionEditService {
   public api:string ="https://ongapi.alkemy.org/api/organization"
+  public img:any='';
+  images: []=[];
   constructor(private http: HttpClient) { 
 
   }
@@ -15,9 +17,28 @@ export class OrganizacionEditService {
   public getOrganization():Observable<any>{
     return (this.http.get<any>(`${this.api}`));
   }
-
+  public getOrganizationId(id:number):Observable<any>{
+    return (this.http.get<any>(`${this.api}/${id}`));
+  }
   public postOrganizacion(organizacion:any):Observable<any>{
     return this,this.http.post<any>(`${this.api}`,organizacion);
   }
-  
+  public putOrganizacion(organizacion:any, id:number):Observable<any>{
+    return this,this.http.put<any>(`${this.api}/${id}`,organizacion);
+  }
+
+
+
+public putImg(event:any){
+  let archivos = event.target.files;  
+  for (let i = 0; i < archivos.length; i++) {
+
+    let reader = new FileReader();
+    reader.readAsDataURL(archivos[0]);
+    reader.onloadend = () => {
+      this.img=reader.result
+      // console.log(this.img);
+    }
+  }
+}
 }
