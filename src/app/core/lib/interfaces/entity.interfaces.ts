6636dcaa2f1674081c9d94f2125e79ab850d80d1
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2022-10-06 21:29:37 -0500

    añadiendo nueva interfaz modelo para la seccion de categorias

--- src/app/core/lib/interfaces/entity.interfaces.ts
+++ src/app/core/lib/interfaces/entity.interfaces.ts
@@ -1,16 +1,20 @@
 /*Aqui se cream todas las interfaces que tengan que ver con objetos que se puedan convertir
 a JSON y asi manejarse*/
 
-export interface Contact{
+//interfaz Padre
+interface Model{
     id:number|null,
-    name:string,
-    email:string,
-    phone:string,
-    message:string,
     deleted_at: Date|null,
     created_at: Date|null,
     updated_at: Date|null
 }
+
+export interface Contact extends Model{
+    name:string,
+    email:string,
+    phone:string,
+}
+
 
 export interface SnackInjectData{
     content:string,
@@ -46,4 +50,11 @@ export interface User{
     longitude:number,
     address:string,
     profile_image:string
+}
+
+export interface Category extends Model{
+    name:string;
+    description:string;
+    image:string;
+    parent_category_id:number
 }
