c410d887abc4bfe59c535d449488c2ea609f89b8
ifrit230 <omar-ortiz@javeriana.edu.co>
2022-10-14 14:21:53 -0500

    Creación de Guards para Restringir acceso a Login y Register después de loggear

--- src/app/core/guards/log-reg.guard.ts
+++ src/app/core/guards/log-reg.guard.ts
@@ -6,16 +6,16 @@ import { AuthFireService } from '../services/auth-fire.service';
 @Injectable({
   providedIn: 'root'
 })
-export class LoginGuard implements CanActivate {
-
+export class LogRegGuard implements CanActivate {
   
   constructor(private authSVC:AuthFireService,private router:Router){}
   canActivate():boolean{
-    if(!this.authSVC.isAuth()){
-       this.router.navigate(['login']);
+    
+    if(this.authSVC.isAuth()){
+       this.router.navigate(['home']);
        return false;
     }
     return true;
   }
-   
+  
 }
