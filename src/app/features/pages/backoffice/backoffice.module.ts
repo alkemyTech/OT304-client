88119b00cd694e1f5c9931ee00d6862b901ecb31
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2022-10-21 18:57:41 -0500

    realizando cambios en importacion de modulos

--- src/app/features/pages/backoffice/backoffice.module.ts
+++ src/app/features/pages/backoffice/backoffice.module.ts
@@ -5,14 +5,11 @@ import { BackofficeComponent } from './backoffice.component';
 import { HomeBackofficeComponent } from './home-backoffice/home-backoffice.component';
 import { CKEditorModule } from 'ckeditor4-angular';
 import { TestimonialFormComponent } from "../../pages/testimonials/testimonial-form/testimonial-form.component";
-import {MatFormFieldModule} from '@angular/material/form-field';
 import { UsersBackofficeComponent } from './users-backoffice/users-backoffice.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NewsFormComponent } from '../news/news-form/news-form.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
+import { SearchFormComponent } from './shared-backoffice/search-form/search-form.component';
+import { MaterialModule } from 'src/app/material.module';
 
 
 
@@ -22,18 +19,15 @@ import {MatIconModule} from '@angular/material/icon';
     HomeBackofficeComponent,
     TestimonialFormComponent,
     UsersBackofficeComponent,
-    NewsFormComponent
+    NewsFormComponent,
+    SearchFormComponent
   ],
   imports: [
     CommonModule,
     BackofficeRoutingModule,
     CKEditorModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
-    MatTableModule,
-    MatCardModule,
-    MatIconModule,
-    MatButtonModule,
+    MaterialModule,
     FormsModule
   ]
 })
