e45ceda8528ac79089fd34c9969a1287a0e43ca7
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2022-10-26 00:11:32 -0300

    ruteo backoffice

--- src/app/features/pages/backoffice/backoffice.component.ts
+++ src/app/features/pages/backoffice/backoffice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-backoffice',
@@ -7,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BackofficeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
     
   }
 
+  irNovedades(){
+    this.router.navigate(['backoffice/news'])
+  }
+
+  irActividades(){
+    this.router.navigate([''])
+  }
+
+
+  irCategorias(){
+    this.router.navigate(['backoffice/categories'])
+  }
+
+  irTestimonios(){
+    this.router.navigate(['/backofficetestimonios'])
+  }
+
+  
+  irOrganizacion(){
+    this.router.navigate([''])
+  }
+
+  irSlides(){
+    this.router.navigate([''])
+  }
+
+  irUsuarios(){
+    this.router.navigate(['backoffice/users'])
+  }
+
+  irMiembros(){
+    this.router.navigate(['members'])
+  }
 }
